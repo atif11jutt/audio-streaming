ead0a174ded181654da3451849730975d30317ca
atif11jutt <atif11jutt@gmail.com>
2021-06-17 14:14:29 +0500

    first upload

---
+++ js/script.js
@@ -0,0 +1,193 @@
+/*
+The MIT License (MIT)
+
+Github: https://github.com/gsavio
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const URL_STREAMING = "http://usa11.fastcast4u.com:5272//;stream.mp3";
+
+window.onload = function () {
+    var page = new Page;
+    page.setVolume();
+
+    var player = new Player();
+    player.play();
+
+
+}
+
+// DOM control
+function Page() {
+
+    this.setVolume = function () {
+        if (typeof (Storage) !== 'undefined') {
+            var volumeLocalStorage = (!localStorage.getItem('volume')) ? 80 : localStorage.getItem('volume');
+            document.getElementById('volume').value = volumeLocalStorage;
+        }
+    }
+
+}
+
+var audio = new Audio(URL_STREAMING);
+
+// Player control
+function Player() {
+    this.play = async function () {
+        await audio.play();
+
+        var defaultVolume = document.getElementById('volume').value;
+
+        if (typeof (Storage) !== 'undefined') {
+            if (localStorage.getItem('volume') !== null) {
+                audio.volume = intToDecimal(localStorage.getItem('volume'));
+            } else {
+                audio.volume = intToDecimal(defaultVolume);
+            }
+        } else {
+            audio.volume = intToDecimal(defaultVolume);
+        }
+    };
+
+    this.pause = function () {
+        audio.pause();
+    };
+}
+
+// On play, change the button to pause
+audio.onplay = function () {
+    var botao = document.getElementById('playerButton');
+
+    if (botao.className === 'play') {
+        botao.className = 'pause';
+    }
+}
+
+// On pause, change the button to play
+audio.onpause = function () {
+    var botao = document.getElementById('playerButton');
+
+    if (botao.className === 'pause') {
+        botao.className = 'play';
+    }
+}
+
+// Unmute when volume changed
+audio.onvolumechange = function () {
+    if (audio.volume > 0) {
+        audio.muted = false;
+    }
+}
+
+audio.onerror = function () {
+    var confirmacao = confirm('Error on communicate to server. \nClick OK to try again.');
+
+    if (confirmacao) {
+        window.location.reload();
+    }
+}
+
+document.getElementById('volume').oninput = function () {
+    audio.volume = intToDecimal(this.value);
+}
+
+function togglePlay() {
+    if (!audio.paused) {
+        audio.pause();
+    } else {
+        audio.load();
+        audio.play();
+    }
+}
+
+function volumeUp() {
+    var vol = audio.volume;
+    if(audio) {
+        if(audio.volume >= 0 && audio.volume < 1) {
+            audio.volume = (vol + .01).toFixed(2);
+        }
+    }
+}
+
+function volumeDown() {
+    var vol = audio.volume;
+    if(audio) {
+        if(audio.volume >= 0.01 && audio.volume <= 1) {
+            audio.volume = (vol - .01).toFixed(2);
+        }
+    }
+}
+
+function mute() {
+    if (!audio.muted) {
+        document.getElementById('volIndicator').innerHTML = 0;
+        document.getElementById('volume').value = 0;
+        audio.volume = 0;
+        audio.muted = true;
+    } else {
+        var localVolume = localStorage.getItem('volume');
+        document.getElementById('volIndicator').innerHTML = localVolume;
+        document.getElementById('volume').value = localVolume;
+        audio.volume = intToDecimal(localVolume);
+        audio.muted = false;
+    }
+}
+
+// Player control by keys
+document.addEventListener('keydown', function (k) {
+    var k = k || window.event;
+    var key = k.keyCode || k.which;
+    
+    var slideVolume = document.getElementById('volume');
+
+    var page = new Page();
+
+    switch (key) {
+        // Arrow up
+        case 38:
+            volumeUp();
+            slideVolume.value = decimalToInt(audio.volume);
+            break;
+        // Arrow down
+        case 40:
+            volumeDown();
+            slideVolume.value = decimalToInt(audio.volume);
+            break;
+        // Spacebar
+        case 32:
+            togglePlay();
+            break;
+        // P
+        case 80:
+            togglePlay();
+            break;
+        // M
+        case 77:
+            mute();
+            break;
+    }
+});
+
+function intToDecimal(vol) {
+    return vol / 100;
+}
+
+function decimalToInt(vol) {
+    return vol * 100;
+}
